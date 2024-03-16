2f5f92302305b8261c1ce7bc49fe5e3b57985087
DolceCasa <103113557+li-peifeng@users.noreply.github.com>
2024-03-17 00:06:18 +0100

    Update setting.go

--- internal/bootstrap/data/setting.go
+++ internal/bootstrap/data/setting.go
@@ -86,9 +86,9 @@ func InitialSettings() []model.SettingItem {
 		{Key: conf.SiteTitle, Value: "DolceCasa", Type: conf.TypeString, Group: model.SITE},
 		{Key: conf.Announcement, Value: "Welcome To DolceCasa", Type: conf.TypeText, Group: model.SITE},
 		{Key: "pagination_type", Value: "all", Type: conf.TypeSelect, Options: "all,pagination,load_more,auto_load_more", Group: model.SITE},
-		{Key: "default_page_size", Value: "30", Type: conf.TypeNumber, Group: model.SITE},
+		{Key: "default_page_size", Value: "50", Type: conf.TypeNumber, Group: model.SITE},
 		{Key: conf.AllowIndexed, Value: "false", Type: conf.TypeBool, Group: model.SITE},
-		{Key: conf.AllowMounted, Value: "true", Type: conf.TypeBool, Group: model.SITE},
+		{Key: conf.AllowMounted, Value: "false", Type: conf.TypeBool, Group: model.SITE},
 		{Key: conf.RobotsTxt, Value: "User-agent: *\nAllow: /", Type: conf.TypeText, Group: model.SITE},
 		// style settings
 		{Key: conf.Logo, Value: "https://github.com/li-peifeng/li-peifeng.github.io/raw/main/logo.svg", Type: conf.TypeText, Group: model.STYLE},
